9f163cc8fdd7bb8ce5e5414313ca07e41135a4ff
Kevas Hans Christian Wihanto <93915338+kevashans@users.noreply.github.com>
2024-10-18 21:05:52 +0100

    54268 - Combined Sensors Script (#1)
    * Initial commit
    
    * Added basic color frequency sensor script
    
    * Converted frequency to RGB values
    
    * Added buzzer logic
    
    * added basic logging
    
    * added device availability monitor script
    
    * Cleanup rgb_sensor
    
    * added wrapper script
    
    * Cleanup
    
    * Added detailed comments

---
+++ sensors_script/device_availability_monitor.py
@@ -0,0 +1,84 @@
+"""
+Module that monitor package installation and control LEDs accordingly  
+"""
+import RPi.GPIO as GPIO
+import time
+import subprocess
+
+GPIO.setmode(GPIO.BCM)
+
+GREEN_LED = 10
+YELLOW_LED = 9
+RED_LED = 11
+
+DPKG_LOG_PATH = '/var/log/dpkg.log'
+UPFLUX_LOG_PATH = '/var/log/upflux.log'
+
+GPIO.setup(GREEN_LED, GPIO.OUT)
+GPIO.setup(YELLOW_LED, GPIO.OUT)
+GPIO.setup(RED_LED, GPIO.OUT)
+
+def set_green_default():
+    GPIO.output(GREEN_LED, GPIO.HIGH)
+    GPIO.output(YELLOW_LED, GPIO.LOW)
+    GPIO.output(RED_LED, GPIO.LOW)
+
+def is_package_installing():
+    """
+    check if an installation is processing by checking for active 'apt' or 'dpkg' processes
+    """
+    try:
+        apt_process = subprocess.run(['pgrep', 'apt'], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        dpkg_process = subprocess.run(['pgrep', 'dpkg'], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        if apt_process.stdout or dpkg_process.stdout:
+            return True
+    except subprocess.SubprocessError as e:
+        print(f"Error checking installation processes: {e}")
+
+    return False
+
+def get_log_tail():
+    """
+    read the last 10 lines of upflux and dpkg log file 
+    """
+    try:
+        result_dpkg = subprocess.run(['tail', '-n', '10', DPKG_LOG_PATH], stdout=subprocess.PIPE, text=True)
+        result_uplflux = subprocess.run(['tail', '-n', '10', UPFLUX_LOG_PATH], stdout=subprocess.PIPE, text=True)
+        return result_dpkg.stdout.lower(), result_uplflux.stdout.lower()
+    except subprocess.SubprocessError as e:
+        print(f"Error reading logs: {e}")
+
+def is_installation_failed(log_data):
+    return "error" in log_data[0] or "error" in log_data[1]
+
+def is_installation_success(log_data):
+    return "status installed" in log_data[0]
+
+try:
+    while True:
+        log_data = get_log_tail() 
+        
+        if is_installation_failed(log_data):
+            GPIO.output(GREEN_LED, GPIO.LOW)
+            GPIO.output(YELLOW_LED, GPIO.LOW)
+            GPIO.output(RED_LED, GPIO.HIGH)
+        elif is_package_installing():
+            GPIO.output(GREEN_LED, GPIO.LOW)
+            GPIO.output(RED_LED, GPIO.LOW)
+            GPIO.output(YELLOW_LED, GPIO.HIGH)
+        elif is_installation_success(log_data):
+            set_green_default()
+        else:
+            set_green_default()
+
+        time.sleep(2)
+
+except KeyboardInterrupt:
+    GPIO.cleanup()
+
+except Exception as e:
+    print(f"An error occurred: {e}")
+    GPIO.cleanup()
+
+finally:
+    GPIO.cleanup()
---
+++ sensors_script/rgb_sensor.py
@@ -0,0 +1,138 @@
+"""
+module for reading color RGB values using TCS3200 Color Sensor and activating a buzzer based on specific RGB values.
+"""
+import RPi.GPIO as GPIO
+import time
+import logging
+
+LOG_FILE = '/var/log/upflux.log'
+logging.basicConfig(filename=LOG_FILE, level=logging.INFO, 
+                    format='%(asctime)s %(levelname)s:%(message)s')
+
+S0_PIN = 23
+S1_PIN = 14
+OE_PIN = 15
+OUT_PIN = 18
+S2_PIN = 25
+S3_PIN = 24
+BUZZER_PIN = 26
+
+NUM_CYCLES = 10 
+RED_THRESHOLD = 200
+GREEN_THRESHOLD = 100
+BLUE_THRESHOLD = 100
+ 
+FREQ_MIN = {'red': 500, 'green': 500, 'blue': 500}
+FREQ_MAX = {'red': 3000, 'green': 3000, 'blue': 3000}  
+
+class ColorSensor:
+    def __init__(self):
+        self.pwm = None
+        self.setup_gpio()
+        
+    def setup_gpio(self):
+        GPIO.setmode(GPIO.BCM)
+        GPIO.setup(OUT_PIN, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+        GPIO.setup(S0_PIN, GPIO.OUT)
+        GPIO.setup(S1_PIN, GPIO.OUT)
+        GPIO.setup(S2_PIN, GPIO.OUT)
+        GPIO.setup(S3_PIN, GPIO.OUT)
+        GPIO.setup(OE_PIN, GPIO.OUT)
+        
+        GPIO.output(OE_PIN, GPIO.LOW)
+        GPIO.output(S0_PIN, GPIO.HIGH)
+        GPIO.output(S1_PIN, GPIO.LOW)
+        GPIO.setup(BUZZER_PIN, GPIO.OUT)
+
+        global pwm
+        pwm = GPIO.PWM(BUZZER_PIN, 1000) 
+        
+    def buzzer_condition(self, red_value, green_value, blue_value):
+        return red_value > RED_THRESHOLD and green_value < GREEN_THRESHOLD and blue_value < BLUE_THRESHOLD
+
+    def activate_buzzer(self):
+        pwm.start(50)  
+        time.sleep(1)  
+        pwm.stop()
+    
+    def map_frequency_to_rgb(self, freq, color):
+        """
+        maps the frequency measured by sensor to an a RGB value(0 - 255).
+        frequency is constrained between max and min rounds
+        """
+        freq_min = FREQ_MIN[color]
+        freq_max = FREQ_MAX[color]
+        freq = max(freq_min, min(freq, freq_max))
+        rgb_value = int((freq - freq_min) / (freq_max - freq_min) * 255)
+        return rgb_value
+    
+    def measure_color_frequency(self, color):
+        """
+        measure the frequency for a color by configuring the sensor's filter
+        and counting the number of pulses within a time frame  
+        """
+        if color == 'red':
+            GPIO.output(S2_PIN, GPIO.LOW)
+            GPIO.output(S3_PIN, GPIO.LOW)
+        elif color == 'blue':
+            GPIO.output(S2_PIN, GPIO.LOW)
+            GPIO.output(S3_PIN, GPIO.HIGH)
+        elif color == 'green':
+            GPIO.output(S2_PIN, GPIO.HIGH)
+            GPIO.output(S3_PIN, GPIO.HIGH)
+
+        time.sleep(0.3)  
+        start = time.time()
+        for _ in range(NUM_CYCLES):
+            GPIO.wait_for_edge(OUT_PIN, GPIO.FALLING)
+        duration = time.time() - start
+        frequency = NUM_CYCLES / duration
+        return frequency
+
+    def loop(self):
+        while True:
+            try:
+                red_freq = self.measure_color_frequency('red')
+                red_value = self.map_frequency_to_rgb(red_freq, 'red')
+                print(f"Red frequency: {red_freq} Hz -> RGB value: {red_value}")
+
+                blue_freq = self.measure_color_frequency('blue')
+                blue_value = self.map_frequency_to_rgb(blue_freq, 'blue')
+                print(f"Blue frequency: {blue_freq} Hz -> RGB value: {blue_value}")
+
+                green_freq = self.measure_color_frequency('green')
+                green_value = self.map_frequency_to_rgb(green_freq, 'green')
+                print(f"Green frequency: {green_freq} Hz -> RGB value: {green_value}")
+
+                print(f"RGB Values -> {red_value}, {green_value}, {blue_value}")
+
+                if self.buzzer_condition(red_value, green_value, blue_value):
+                    print("Buzzer activated")
+                    logging.info(
+                        f"buzzer activated due to color condition "
+                        f"(R > {RED_THRESHOLD}, G < {GREEN_THRESHOLD}, B < {BLUE_THRESHOLD})"
+                    )
+                    self.activate_buzzer()
+
+                time.sleep(2)
+
+            except Exception as e:
+                logging.error(f"error occurred: {e}", exc_info=True)
+                time.sleep(1)
+
+    def cleanup(self):
+        GPIO.cleanup()
+        logging.info("RGB sensor shutdown")
+
+
+def main():
+    sensor = ColorSensor()
+    try:
+        sensor.loop()
+    except KeyboardInterrupt:
+        logging.info("Program interrupted")
+    finally:
+        sensor.cleanup()
+
+if __name__ == '__main__':
+    main()
