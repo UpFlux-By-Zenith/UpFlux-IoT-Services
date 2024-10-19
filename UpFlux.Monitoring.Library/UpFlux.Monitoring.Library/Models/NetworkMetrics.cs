40ce6a8afde7ac02d7c5d425cd76f650df605027
Patrick Orjieh <orjiehpatrick@gmail.com>
2024-10-19 17:37:19 +0100

    Add the model for the network metrics

---
+++ UpFlux.Monitoring.Library/UpFlux.Monitoring.Library/Models/NetworkMetrics.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UpFlux.Monitoring.Library.Models
+{
+    /// <summary>
+    /// Represents network metrics such as received and transmitted data.
+    /// </summary>
+    public class NetworkMetrics
+    {
+        /// <summary>
+        /// Bytes received over the network
+        /// </summary>
+        public long ReceivedBytes { get; set; }
+        /// <summary>
+        /// Bytes transmitted over the network
+        /// </summary>
+        public long TransmittedBytes { get; set; }
+    }
+}
