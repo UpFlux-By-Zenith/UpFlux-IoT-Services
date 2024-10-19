2543a8c4bd568bae80c123a47b23e359131e94ec
Patrick Orjieh <orjiehpatrick@gmail.com>
2024-10-19 17:41:20 +0100

    Add the model for the network bandwith metrics

---
+++ UpFlux.Monitoring.Library/UpFlux.Monitoring.Library/Models/BandwidthMetrics.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UpFlux.Monitoring.Library.Models
+{
+    /// <summary>
+    /// Represents network bandwidth metrics.
+    /// </summary>
+    public class BandwidthMetrics
+    {
+        /// <summary>
+        /// Download speed in Mbps
+        /// </summary>
+        public double DownloadSpeed { get; set; }
+        /// <summary>
+        /// Upload speed in Mbps
+        /// </summary>
+        public double UploadSpeed { get; set; }
+    }
+}
