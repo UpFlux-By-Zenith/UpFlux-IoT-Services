ad58ef3ee6eab2d85e964b90f1b3fbc12f811fbe
Patrick Orjieh <orjiehpatrick@gmail.com>
2024-10-19 17:33:34 +0100

    Add the model for the cpu metrics

---
+++ UpFlux.Monitoring.Library/UpFlux.Monitoring.Library/Models/CpuMetrics.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UpFlux.Monitoring.Library.Models
+{
+    /// <summary>
+    /// Represents the CPU metrics such as current usage and load average.
+    /// </summary>
+    public class CpuMetrics
+    {
+        /// <summary>
+        /// CPU usage in percentage
+        /// </summary>
+        public double CurrentUsage { get; set; }
+        /// <summary>
+        /// Load average over a given time period
+        /// </summary>
+        public double LoadAverage { get; set; }
+    }
+}
