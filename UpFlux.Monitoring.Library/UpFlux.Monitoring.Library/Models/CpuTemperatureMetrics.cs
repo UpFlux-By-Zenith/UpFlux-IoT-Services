c1b979c7ce54db647ff1a906dc45ce0ae899c321
Patrick Orjieh <orjiehpatrick@gmail.com>
2024-10-19 17:40:21 +0100

    Add the model for the system uptime and the cpu temperature metrics

---
+++ UpFlux.Monitoring.Library/UpFlux.Monitoring.Library/Models/CpuTemperatureMetrics.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UpFlux.Monitoring.Library.Models
+{
+    /// <summary>
+    /// Represents CPU temperature in degrees Celsius.
+    /// </summary>
+    public class CpuTemperatureMetrics
+    {
+        /// <summary>
+        /// CPU temperature in degrees Celsius
+        /// </summary>
+        public double TemperatureCelsius { get; set; }
+    }
+}
---
+++ UpFlux.Monitoring.Library/UpFlux.Monitoring.Library/Models/SystemUptimeMetrics.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UpFlux.Monitoring.Library.Models
+{
+    /// <summary>
+    /// Represents system uptime in seconds.
+    /// </summary>
+    public class SystemUptimeMetrics
+    {
+        /// <summary>
+        /// System uptime in seconds
+        /// </summary>
+        public long UptimeSeconds { get; set; }
+    }
+}
