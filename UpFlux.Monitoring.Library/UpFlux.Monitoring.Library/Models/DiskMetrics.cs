c3dbb23e305f366cbd7fb94c6713f530349a92f2
Patrick Orjieh <orjiehpatrick@gmail.com>
2024-10-19 17:38:53 +0100

    Add the model for the disk usage metrics

---
+++ UpFlux.Monitoring.Library/UpFlux.Monitoring.Library/Models/DiskMetrics.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UpFlux.Monitoring.Library.Models
+{
+    /// <summary>
+    /// Represents disk usage metrics such as total space, free space, and used space.
+    /// </summary>
+    public class DiskMetrics
+    {
+        /// <summary>
+        /// Total disk space in bytes
+        /// </summary>
+        public long TotalDiskSpace { get; set; }
+        /// <summary>
+        /// Free disk space in bytes
+        /// </summary>
+        public long FreeDiskSpace { get; set; }
+        /// <summary>
+        /// Used disk space in bytes
+        /// </summary>
+        public long UsedDiskSpace => TotalDiskSpace - FreeDiskSpace;
+    }
+}
