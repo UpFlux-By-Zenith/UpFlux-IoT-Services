bb8445508bd96e5c8c8fba48c6033af3661b6938
Patrick Orjieh <orjiehpatrick@gmail.com>
2024-12-01 18:20:57 +0000

    Update the appsettings and the configuration model for the Update service

--- UpFlux.Update.Service/UpFlux.Update.Service/Models/Configuration.cs
+++ UpFlux.Update.Service/UpFlux.Update.Service/Models/Configuration.cs
@@ -1,8 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace UpFlux.Update.Service.Models
 {
@@ -11,17 +8,50 @@ namespace UpFlux.Update.Service.Models
     /// </summary>
     public class Configuration
     {
+        [Required]
         public string GatewayServerIp { get; set; }
+
+        [Required]
         public int GatewayServerPort { get; set; }
+
+        [Required]
         public string GatewayServerLogEndpoint { get; set; }
+
+        [Required]
         public string IncomingPackageDirectory { get; set; }
+
+        [Required]
         public string PackageDirectory { get; set; }
+
         public int MaxStoredVersions { get; set; }
+
+        [Required]
         public string MonitoringServiceLog { get; set; }
+
+        [Required]
         public string UpdateServiceLog { get; set; }
+
         public int SimulationTimeoutSeconds { get; set; }
+
         public int PostInstallationMonitoringMinutes { get; set; }
+
         public List<string> ErrorPatterns { get; set; }
+
+        [Required]
         public string PackageNamePattern { get; set; }
+
+        [Required]
+        public string CertificatePath { get; set; }
+
+        public string CertificatePassword { get; set; }
+
+        [Required]
+        public string TrustedCaCertificatePath { get; set; }
+
+        [Required]
+        public string DeviceUuid { get; set; }
+
+        [Required]
+        public string LicenseFilePath { get; set; }
     }
 }
