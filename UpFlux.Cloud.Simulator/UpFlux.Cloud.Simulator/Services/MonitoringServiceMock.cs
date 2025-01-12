924bff1ab3591430fdf9c7bd27b1dd941e96c699
Patrick Orjieh <orjiehpatrick@gmail.com>
2025-01-12 05:38:17 +0000

    Implement the MonitoringServiceMock to log the data coming from the aggregated devices

--- UpFlux.Cloud.Simulator/UpFlux.Cloud.Simulator/Services/MonitoringServiceMock.cs
+++ UpFlux.Cloud.Simulator/UpFlux.Cloud.Simulator/Services/MonitoringServiceMock.cs
@@ -1,12 +1,35 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+﻿using Grpc.Core;
+using Microsoft.Extensions.Logging;
 using System.Threading.Tasks;
+using UpFlux.Gateway.Server.Protos;
 
-namespace UpFlux.Cloud.Simulator.Services
+namespace UpFlux.Cloud.Simulator
 {
-    internal class MonitoringServiceMock
+    public class MonitoringServiceMock : MonitoringService.MonitoringServiceBase
     {
+        private readonly ILogger<MonitoringServiceMock> _logger;
+
+        public MonitoringServiceMock(ILogger<MonitoringServiceMock> logger)
+        {
+            _logger = logger;
+        }
+
+        public override Task<AggregatedDataResponse> SendAggregatedData(AggregatedDataRequest request, ServerCallContext context)
+        {
+            _logger.LogInformation("CloudSim: Received {count} AggregatedData items.", request.AggregatedDataList.Count);
+
+            foreach (AggregatedData? data in request.AggregatedDataList)
+            {
+                _logger.LogInformation(" -> Device={0}, CPU={1}% Mem={2}%",
+                    data.Uuid, data.Metrics.CpuUsage, data.Metrics.MemoryUsage);
+            }
+
+            AggregatedDataResponse resp = new AggregatedDataResponse
+            {
+                Success = true,
+                Message = "CloudSim: Data logged"
+            };
+            return Task.FromResult(resp);
+        }
     }
 }
