c58276fdb3bab640393939425df7459b5ccf0f37
Patrick Orjieh <orjiehpatrick@gmail.com>
2025-01-12 05:28:32 +0000

    Include all th needed services stubs in the cloud simulator

---
+++ UpFlux.Cloud.Simulator/UpFlux.Cloud.Simulator/Services/AlertServiceMock.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UpFlux.Cloud.Simulator.Services
+{
+    internal class AlertServiceMock
+    {
+    }
+}
