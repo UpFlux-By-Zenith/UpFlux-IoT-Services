0031e105c7d5173da908e8f213debe6cefee28a5
Patrick Orjieh <orjiehpatrick@gmail.com>
2025-01-12 05:34:13 +0000

    Implement the AlerServiceMock to just the notifications sent from the Gateway

--- UpFlux.Cloud.Simulator/UpFlux.Cloud.Simulator/Services/AlertServiceMock.cs
+++ UpFlux.Cloud.Simulator/UpFlux.Cloud.Simulator/Services/AlertServiceMock.cs
@@ -1,12 +1,29 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+﻿using Grpc.Core;
+using Microsoft.Extensions.Logging;
 using System.Threading.Tasks;
+using UpFlux.Gateway.Server.Protos;
 
-namespace UpFlux.Cloud.Simulator.Services
+namespace UpFlux.Cloud.Simulator
 {
-    internal class AlertServiceMock
+    public class AlertServiceMock : AlertService.AlertServiceBase
     {
+        private readonly ILogger<AlertServiceMock> _logger;
+
+        public AlertServiceMock(ILogger<AlertServiceMock> logger)
+        {
+            _logger = logger;
+        }
+
+        public override Task<AlertResponse> SendAlert(AlertRequest request, ServerCallContext context)
+        {
+            _logger.LogInformation("CloudSim ALERT from {0} | Level={1} | Msg={2}",
+                request.Source, request.Level, request.Message);
+
+            return Task.FromResult(new AlertResponse
+            {
+                Success = true,
+                Message = "CloudSim: alert received"
+            });
+        }
     }
 }
